1ebc9cdda1f7263858942e8695b653bb2ce691c1
unknown <you@example.com>
2017-06-05 02:18:08 -0500

    cambios

--- js/app.js
+++ js/app.js
@@ -10,38 +10,38 @@ var aletorio= setInterval(imgSecuencia, 3000);
       //circulos//
 
 $("#c-1").on("click",function(){
-  $('.carrusel div:first-child').fadeOut(2000)
+  $('.carrusel div:first-child').fadeOut(3000)
    $('#img-1').fadeIn(1000)
    .end().appendTo('.carrusel');
    });
    $("#c-2").on("click",function(){
-     $('.carrusel div:first-child').fadeOut(2000)
+     $('.carrusel div:first-child').fadeOut(3000)
        $('#img-2').fadeIn(1000)
        .end().appendTo('.carrusel');
    });
    $("#c-3").on("click",function(){
-     $('.carrusel div:first-child').fadeOut(2000)
+     $('.carrusel div:first-child').fadeOut(3000)
        $('#img-3').fadeIn(1000)
       .end().appendTo('.carrusel');
    });
    $("#c-4").on("click",function(){
-     $('.carrusel div:first-child').fadeOut(2000)
+     $('.carrusel div:first-child').fadeOut(3000)
        $('#img-4').fadeIn(1000)
       .end().appendTo('.carrusel');
    });
    $("#c-5").on("click",function(){
-     $('.carrusel div:first-child').fadeOut(2000)
+     $('.carrusel div:first-child').fadeOut(3000)
        $('#img-5').fadeIn(1000)
     .end().appendTo('.carrusel');
    });
 
 //Flechas//
-   $("#antes").click(function(){
+   $("#antes").on("click",function(){
          imgSecuencia();
          clearInterval(aletorio);
          aletorio =setInterval(imgSecuencia,5000);
        });
-       $("#despues").click(function(){
+       $("#despues").on("cllick",function(){
          $('.carrusel div:first-child').fadeOut(3000);
          $('.carrusel div:last-child').fadeIn(3000).prependTo('.carrusel');
          clearInterval(aletorio);
